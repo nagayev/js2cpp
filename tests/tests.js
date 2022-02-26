3a754188e3280f5bcd4cdf9610a204e05dc94e00
Marat Nagayev <nagaevmt49@gmail.com>
2022-02-27 01:32:08 +0300

    Improve tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,13 @@
+function test(){
+    console.log('Hello world');
+}
+test();
 let a = 2;
-let b = 5;
-a = b;
-console.log(a);
+let b = 2;
+b = a;
+let c = [1,2,3];
+c = [4,5];
+let i = 0;
+for(;i<5;i++){
+    console.log(i);
+}
