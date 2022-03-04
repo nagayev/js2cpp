d26e36d52935eb62e88694760adea87e09391e32
Marat Nagayev <nagaevmt49@gmail.com>
2022-03-04 16:35:14 +0300

    Move `console` and `math` folders to `stdlib`

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 echo "Creating output"
 mkdir output
+cp -r stdlib/math output/math
+cp -r stdlib/console output/console
 echo "Compiling correct.js"
 node js2cpp.js tests/correct.js
 echo "Compiling incorrect.js"
