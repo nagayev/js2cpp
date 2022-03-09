91bc3cf987716baa76e076fc9bd6cf35fa733756
Marat Nagayev <nagaevmt49@gmail.com>
2022-03-10 00:20:22 +0300

    [WORKFLOW] Delete `stdlib` folder

--- compile.sh
+++ compile.sh
@@ -1,23 +1,35 @@
 echo "Creating output"
 mkdir output
 cp -r stdlib output/stdlib
-echo "Compiling correct tests"
+
+echo "Compiling correct tests..."
+
+all_tests=$(ls tests/correct | wc -l)
+i=1
 for file in $(ls tests/correct)
 do
 cpp_ext="${file%.js}.cpp"
+echo Test $i/$all_tests
 node js2cpp.js tests/correct/$file  --output output/$cpp_ext
+i=$(($i+1));
 done
-echo "Compiling incorrect tests"
+
+echo "Compiling incorrect tests..."
+
+all_tests=$(ls tests/incorrect | wc -l)
+i=1
 for file in $(ls tests/incorrect)
 do
+echo Test $i/$all_tests
 node js2cpp.js tests/incorrect/$file  --output output/js_result.cpp #don't produce js_result.cpp
 # panic if something is ok
 if [[ $? -eq 0 ]]
 then
   exit 1
 fi
+i=$(($i+1));
 done
-echo "Calling cpp compiler"
+
 for file in $(ls output/ | grep .cpp)
 do
 without_ext="${file%.cpp}"
@@ -26,10 +38,9 @@ g++ output/$file -o output/$without_ext
 if [[ $? -gt 0 ]]
 then
   exit 1
-else
-  exit 0
 fi
 done
+
 echo "Deleting stdlib folder"
 rm -rf output/stdlib #exclude stdlib from output
  
