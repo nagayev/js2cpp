ebc8807f135699e23b41895d88c2600fa78a1b2b
Marat Nagayev <nagaevmt49@gmail.com>
2022-03-09 23:53:12 +0300

    New tests

--- compile.sh
+++ compile.sh
@@ -1,19 +1,35 @@
 echo "Creating output"
 mkdir output
 cp -r stdlib output/stdlib
-echo "Compiling correct.js"
-node js2cpp.js tests/correct.js  --output output/js_result.cpp
-echo "Compiling incorrect.js"
-node js2cpp.js tests/incorrect.js  --output output/js_result.cpp #don't produce js_result.cpp
-saved_exit_code=$?
-echo "Calling g++"
-g++ output/js_result.cpp -o output/js_bin
-echo "Deleting stdlib folder"
-rm -rf output/stdlib #exclude stdlib from output
-if [[ saved_exit_code -gt 0 ]]
+echo "Compiling correct tests"
+for file in $(ls tests/correct)
+do
+cpp_ext="${file%.js}.cpp"
+node js2cpp.js tests/correct/$file  --output output/$cpp_ext
+done
+echo "Compiling incorrect tests"
+for file in $(ls tests/incorrect)
+do
+node js2cpp.js tests/incorrect/$file  --output output/js_result.cpp #don't produce js_result.cpp
+# panic if something is ok
+if [[ $? -eq 0 ]]
 then
-  exit 0
-else
   exit 1
 fi
+done
+echo "Calling cpp compiler"
+for file in $(ls output/ | grep .cpp)
+do
+without_ext="${file%.cpp}"
+g++ output/$file -o output/$without_ext
+# panic if something went wrong
+if [[ $? -gt 0 ]]
+then
+  exit 1
+else
+  exit 0
+fi
+done
+echo "Deleting stdlib folder"
+rm -rf output/stdlib #exclude stdlib from output
  
---
+++ tests/incorrect/incorrect_array.js
@@ -0,0 +1,1 @@
+const arr = [1,'hello',2]; 
