496f1c4dafe53d41693e1a74aba584f7d8848e72
Marat Nagayev <nagaevmt49@gmail.com>
2022-03-09 12:40:21 +0300

    Exclude stdlib folder from output

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 echo "Creating output"
 mkdir output
-cp -r stdlib/math output/math
-cp -r stdlib/console output/console
+cp -r stdlib output/stdlib
 echo "Compiling correct.js"
-node js2cpp.js tests/correct.js  --output output/js_result.cpp -s .
+node js2cpp.js tests/correct.js  --output output/js_result.cpp
 echo "Compiling incorrect.js"
-node js2cpp.js tests/incorrect.js  --output output/js_result.cpp -s . #don't produce js_result.cpp
+node js2cpp.js tests/incorrect.js  --output output/js_result.cpp #don't produce js_result.cpp
+rm -rf output/stdlib #exclude stdlib from output
 if [[ $? -gt 0 ]]
 then
   exit 0
